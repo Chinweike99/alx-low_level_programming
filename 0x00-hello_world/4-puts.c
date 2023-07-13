5425c3c066af04b8558e9bade7b7e8fa6d0945f2
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-13 11:36:29 -0700

    COmipile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
 *main - The main entry
-*return : always o(Success)
+*
+*return : always 0 (Success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
