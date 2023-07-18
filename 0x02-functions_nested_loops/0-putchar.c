f375298d3bf6877a7b55fc332ebed890cba0d69f
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-18 11:52:42 -0700

    loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - check the codes
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
-	char str[100] = "_putchar";
-
-	for (int i = 0; i < strlen(str); i++)
-	{
-		putchar(str[i]);
-	}
-	putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
