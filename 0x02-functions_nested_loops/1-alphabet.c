d5a54bdbebb2e14622e19e6a314d064aa5c398bf
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-18 12:26:57 -0700

    Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  * Return: Always 0
  */
 void print_alphabet(void);
-int main(void)
+int main()
 {
 	print_alphabet();
 	return (0);
 
 }
-void print_alphabet(void)
+void print_alphabet()
 {
 	int a;
 
