e2e31c574889b58f367f8876741f00fdcd50a110
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-18 13:33:49 -0700

    Function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * Main - Check the codes
  *
  * Return: Always 0
  */
-void print_alphabet();
-int main()
-{
-	print_alphabet();
-	return (0);
-
-}
-void print_alphabet()
+void print_alphabet(void)
 {
 	int a;
 
