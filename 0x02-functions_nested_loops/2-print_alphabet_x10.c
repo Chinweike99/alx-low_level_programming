099184b7aa7d257ef2195087c6bab85db7754a2e
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-18 14:25:33 -0700

    Function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Print Lowercase alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char num, alpha;
+
+	for (num = 0; num <= 9; num ++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			putchar(alpha);
+		}
+		putchar('\n');
+	}
+}
+
