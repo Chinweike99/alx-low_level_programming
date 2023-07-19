f21e288e22017f03c7323960d57c3f12c8ebe67a
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-18 17:06:49 -0700

    Func

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - sum to integers and return the result
+ * @x: Number to be addeded
+ * @y: Number to be addeded
+ * Return: Always 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
