0c195c6ab12c62b7392caddfc517e35f49a5ed9b
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-21 11:18:36 -0700

    Alx

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - a function that checks for a digit (0 through 9)
+ * @c: The character to be checked
+ * Return: Return 1 for digit and 0 for anything else
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
