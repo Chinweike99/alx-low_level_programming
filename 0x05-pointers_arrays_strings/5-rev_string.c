454a4209e7b3f5ae9ea288556798ecf007d69c69
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-24 15:03:54 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * rev_string - Reverse a string
+ * @s: Input String
+ * Retur: Return String
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\n')
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
