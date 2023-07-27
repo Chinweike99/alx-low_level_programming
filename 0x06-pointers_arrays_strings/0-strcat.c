91276e1c25b8deef88e3dd1c107c6d9e3d3b2f6f
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-27 04:50:55 -0700

    POinters

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char s1[98] = "Hello";
-	char s2[] = "World";
+	int len1;
+	int i;
 
-	strcat(s1, s2);
-	printf("%s", s1);
+	for (i = 0; i <= len1; i++)
+		dest[len1 + i] = src[i];
+	printf("%s", dest);
 }
