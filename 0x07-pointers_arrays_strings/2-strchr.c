5764e623ead118e8974f59e0429bcc2589e8a3c8
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-31 07:58:08 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include <stddef.h>
+#include <string.h>
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: The String of charcter
+ * @c: The character to be located
+ * Return: Always success
+ */
+char *_strchr(char *s, char c)
+{
+	char *f = "llo";
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+		if (f != NULL)
+		{
+			_putchar(*f);
+		}
+	}
+	return (0);
+}
