6f51163b119de81f72f88bf07ed6cb6be76e1893
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-07-31 17:07:39 -0700

    Pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strspn - a function that gets the length of a prefix substring.
+ * @s: a pointer to the string for which we want to find the length
+ * @accept: pointer to the string containing characters
+ * Return: Return string s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		for (j = 0; *accept != '\0'; j++)
+		{
+			return (*accept);
+		}
+		return (*s);
+	}
+	return (0);
+}
