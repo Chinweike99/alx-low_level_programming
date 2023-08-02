e0f27e69c4b6884b773302d4b73bca5efcf635fd
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-02 13:28:23 -0700

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - A functionn that returns prime numbers
+ * @n: The integer to be returned
+ * Return: Always success
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 != 0)
+	{
+		return (1);
+	}
+	return (0);
+}
