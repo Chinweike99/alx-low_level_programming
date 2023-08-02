1d5d43a85a305daeaa457717ef045fd2eb916335
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-02 09:46:08 -0700

    Recursions

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - a function that returns the factorial of a given number
+ * @n: The value to be returned
+ * Return: Always Success
+ */
+int factorial(int n)
+{
+	int s;
+
+	if (n < 0)
+	{
+		return (n);
+	}
+	s = n * factorial(n - 1);
+		return (s);
+}
