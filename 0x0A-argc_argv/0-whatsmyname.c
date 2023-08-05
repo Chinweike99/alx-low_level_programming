d54b66101ae415747441f024c10a04c5d765cf1c
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-04 17:48:24 -0700

    Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The Entry Point
+ * @argc: he number of command-line arguments passed
+ * @argv: An array of strings containing the command-line arguments
+ * Return: Always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf(" %s\n", argv[0]);
+	return (0);
+}
