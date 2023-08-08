7da5e05ed8c065fc5f68f1672dfc1d393b9c4c77
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-08 12:23:57 -0700

    Malloc0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int n;
 
-	ptr =(char *) malloc(size * sizeof(char));
+	ptr =malloc(size * sizeof(char));
 	for (n = 0; n < size; n++)
 	{
 		ptr[n] = c;
