8f28f1c17f7a5ba60c0e3fc367568dd221631c8a
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-10 15:33:35 -0700

    Malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr;
 
-	ptr = (int *) malloc(nmemb * sizeof(char));
-	if (ptr != NULL)
-	{
-		return (0);
-	}
 	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (int *) malloc(nmemb * sizeof(char));
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
