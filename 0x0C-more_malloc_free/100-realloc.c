d6e8fbc7b98eb0163bc137ba8fedaef174625f6b
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-11 05:12:04 -0700

    Malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int copy_size;
 
 	new_size = 2 * old_size;
-	ptr = (int *)malloc(old_size * sizeof(int));
+	ptr = malloc(old_size * sizeof(int));
 	newptr = (int *)realloc(ptr, new_size * sizeof(int));
 	copy_size = old_size < new_size ? old_size : new_size;
 	if (newptr != NULL)
@@ -33,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size > old_size)
 	{
-		newptr = NULL;
+		newptr = ptr;
 	}
 	if (new_size == old_size)
 	{
