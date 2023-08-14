f48e1029dcdae6ef101911a7e2ad9aed8fa2781b
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-14 12:21:09 -0700

    Struct0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include "dog.h"
+/**
+ * struct dog - The data type
+ * @name: Charcter name
+ * @age: Age of dog
+ * @owner: Name of owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+#endif
