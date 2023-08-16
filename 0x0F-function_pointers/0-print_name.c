cd1974bcee3ca31f360f95c2c9dade0fba77a5bb
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-16 06:29:36 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - Function that prints a name
+ * @name: point to name needed
+ * @f: Takes name parameter
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
