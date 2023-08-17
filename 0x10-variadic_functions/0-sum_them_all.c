249716ca7cc9685a11cf2faca8aef28e24548b5c
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-17 15:34:27 -0700

    VF

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,10 +1,12 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
  * sum_them_all - function that returns the sum of all its parameters.
  * @n: The first parameter
- * Return: Return Success
  * @all: Name of list
  * @k: For loop
+ *
+ * Return: Return sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
