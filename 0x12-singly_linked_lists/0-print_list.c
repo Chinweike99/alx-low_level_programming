42758162467573305e65e3b16bcabf669239c27c
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-24 13:40:54 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,10 @@
 #include "lists.h"
 
 /**
- * print_list - function that prints all
- * the elements of a list_t list.
+ * print_list - function that prints all the elements of a list_t list.
  * @h: A pointer to the list to be printed
  * Return: Return number of nodes on Success
  */
-
 size_t print_list(const list_t *h)
 {
 	int d;
