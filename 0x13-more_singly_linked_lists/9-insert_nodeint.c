27c33f1662782103e7f8d1cbf42bf03cbaebc06d
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-08-29 10:40:35 -0700

    INSERT

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,51 +11,32 @@ listint_t create_node(int n);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int x;
-	listint_t *temp;
-	listint_t *prev;
-	listint_t *my_node;
+	listint_t *newnode;
+	listint_t *my_temp;
 
-	temp = *head;
-	if (head == NULL)
+	my_temp = *head;
+	newnode = malloc(sizeof(listint_t));
+	if (!newnode || !head)
 		return (NULL);
-	/*my_node = create_node(n);*/
-	if (my_node == NULL)
-		return (NULL);
-	if (*head == NULL)
-	{
-		*head = my_node;
-		return (my_node);
-	}
+	newnode->n = n;
+	newnode->next = NULL;
+
 	if (idx == 0)
-		*head = my_node;
-	for (x = 0; x < idx - 1;/* &&temp != NULL && idx != 0;*/ x++)
-		temp = temp->next;
-	if (temp == NULL)
-		return (NULL);
-	if (idx == 0)
-		my_node->next = temp;
-	else
 	{
-		prev = temp->next;
-		temp->next = my_node;
-		my_node->next = prev;
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
 	}
-	return (my_node);
-}
-
-/**
- * create_note - Function to create new node
- * @n: takes the index of the nodes
- * Return: return address of node
- */
-listint_t create_node(int n)
-{
-	listint_t *my_node;
-
-	my_node = malloc(sizeof(listint_t));
-	if (my_node == NULL)
-		return;
-	my_node->n = n;
-	my_node->next = NULL;
-	return (my_node);
+	for (x = 0; my_temp && x < idx; x++)
+	{
+		if (x == idx - 1)
+		{
+			newnode->next = my_temp->next;
+			my_temp->next = newnode;
+			return (newnode);
+		}
+		else
+			my_temp = my_temp->next;
+	}
+	return (NULL);
 }
