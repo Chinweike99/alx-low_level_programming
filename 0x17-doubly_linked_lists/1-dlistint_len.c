ceb51afa7aa1171b3ac4ab3f1727dba872c4479e
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-10-13 04:18:49 -0700

    Return number of elemenmts

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/*
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: The head of the list
+ * Return: The number of nodes
+
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int x = 0;
+
+	if (h == NULL)
+		return(x);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
