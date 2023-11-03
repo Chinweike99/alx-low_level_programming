20a6396630ad8d5ef48d6f7aee7d2943c981d9b8
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-11-03 13:42:00 -0700

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,11 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *hashTable;
+	hash_table_t *hashTable;
 
-        hashTable = malloc(sizeof(hash_table_t));
-        hashTable->array = malloc(sizeof(hash_table_t **) * TABLE_SIZE);
-        for (size = 0; size < TABLE_SIZE; size++)
-                hashTable->array[size] = NULL;
-        return (hashTable);
+	hashTable = malloc(sizeof(hash_table_t));
+	hashTable->array = malloc(sizeof(hash_table_t **) * TABLE_SIZE);
+	for (size = 0; size < TABLE_SIZE; size++)
+		hashTable->array[size] = NULL;
+	return (hashTable);
 }
