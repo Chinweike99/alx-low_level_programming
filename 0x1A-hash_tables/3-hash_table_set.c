be1d3b7ffa93c2bfde5574d51cd52e5f92fac74a
Akwolu Innocent <Innocentakwolu1@gmail.com>
2023-11-03 14:34:29 -0700

    Add Table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,63 +1,50 @@
 #include "hash_tables.h"
 
 /**
- * add_n_hash - adds a node at the beginning of a hash at a given index
- *
- * @head: head of the hash linked list
- * @key: key of the hash
- * @value: value to store
- * Return: head of the hash
+ * hash_table_set - adds item in a hash table
+ * @ht: the table
+ * @key: key of the item
+ * @value: value of the item
+ * Return: 1 for success 0 for failure
  */
-hash_node_t *add_n_hash(hash_node_t **head, const char *key, const char *value)
-{
-	hash_node_t *tmp;
-	tmp = *head;
 
-	while (tmp != NULL)
-	{
-		if (strcmp(key, tmp->key) == 0)
-		{
-			free(tmp->value);
-			tmp->value = strdup(value);
-			return (*head);
-		}
-		tmp = tmp->next;
-	}
-	tmp = malloc(sizeof(hash_node_t));
-
-	if (tmp == NULL)
-		return (NULL);
-
-	tmp->key = strdup(key);
-	tmp->value = strdup(value);
-	tmp->next = *head;
-	*head = tmp;
-
-	return (*head);
-}
-
-/**
- * hash_table_set - adds a hash (key, value) to a given hash table
- *
- * @ht: pointer to the hash table
- * @key: key of the hash
- * @value: value to store
- * Return: 1 if successes, 0 if fails
- */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int k_index;
+	unsigned long int i;
+	hash_node_t *new, *tmp;
 
-	if (ht == NULL)
+	if (!ht || !key || !(*key))
 		return (0);
-
-	if (key == NULL || *key == '\0')
+	new = malloc(sizeof(*new));
+	if (!new)
 		return (0);
-
-	k_index = key_index((unsigned char *)key, ht->size);
-
-	if (add_n_hash(&(ht->array[k_index]), key, value) == NULL)
-		return (0);
+	new->key = strdup((char *)key);
+	new->value = strdup(value);
+	new->next = NULL;
+	i = key_index((unsigned char *)key, ht->size);
+	if (!(ht->array[i]))
+	{
+		ht->array[i] = new;
+	}
+	else
+	{
+		/* check for update */
+		tmp = ht->array[i];
+		while (tmp && strcmp(tmp->key, new->key) != 0)
+			tmp = tmp->next;
+		if (tmp) /* its update */
+		{
+			free(tmp->value);
+			tmp->value = new->value;
+			free(new->key);
+			free(new);
+			return (1);
+		}
+		/* its a collision */
+		tmp = ht->array[i];
+		new->next = tmp;
+		ht->array[i] = new;
+	}
 
 	return (1);
 }
